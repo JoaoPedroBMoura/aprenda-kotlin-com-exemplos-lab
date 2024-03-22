07a6be0a00bcaddb24c2fa8aac02e7e2bc67a392
joaoPedroBM <joaopedrobarcelllosmoura@gmail.com>
2024-03-22 19:35:20 -0300

    fix:separating EducationalContent class

--- src/challenge.kt
+++ src/challenge.kt
@@ -4,7 +4,6 @@ enum class Level { BASIC, INTERMEDIARY, ADVANCED }
 
 enum class EnrollmentStatus {NOT_ENROLLED, ENROLLED}
 
-data class EducationalContent(val name: String, var duration: Int = 60, var technologies: String, var description : String)
 
 
 
